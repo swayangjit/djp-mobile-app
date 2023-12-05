2d58244e2c3c774224500249114e970e9515d4c2
vpPavithra <pavithra.prakash@tarento.com>
2023-12-05 14:32:17 +0530

    Updated project structure and added android platform

---
+++ src/app/mypitara/mypitara.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mypitara',
+  templateUrl: 'mypitara.page.html',
+  styleUrls: ['mypitara.page.scss']
+})
+export class MyPitaraPage {
+
+  constructor() {}
+
+}
