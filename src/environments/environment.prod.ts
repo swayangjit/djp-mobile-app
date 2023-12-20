441cb5e1c8879c40fc0a07c7c50ea39bdcb9bf4d
vpPavithra <pavithra.prakash@tarento.com>
2023-12-20 19:01:10 +0530

    Bot api implementation and search page audio fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,11 +5,12 @@ export const environment = {
 export const apiConfig = {
   BASE_URL: 'http://152.67.162.156:9000/',
   SEARCH_BASE_URL: 'http://129.154.42.158:8000/',
+  BOT_BASE_URL: 'http://144.24.130.223:7081/',
   CONFIG: 'v1/config/read',
   PAGE_SEARCH_API: 'v1/page/search',
   CONTEXT_SEARCH: 'v1/context',
   CONTENT_SEARCH_API: 'v1/content/search',
   TELEMETRY_SYNC: 'v1/telemetry',
-  BOT_QUERY_API: '/v1/query',
+  BOT_QUERY_API: 'v1/query',
   SEARCH_API: 'api/content/v1/search'
 }
