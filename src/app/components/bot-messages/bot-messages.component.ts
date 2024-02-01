26f1e503aa2511d0b3ca29d2a9bef9a45f47e172
vpPavithra <pavithra.prakash@tarento.com>
2024-02-01 11:48:10 +0530

    audio pause on device back button

--- src/app/components/bot-messages/bot-messages.component.ts
+++ src/app/components/bot-messages/bot-messages.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, Input, NgZone, OnInit, Output, ViewChild } from '@angular/core';
-import { IonContent } from '@ionic/angular';
+import { IonContent, Platform } from '@ionic/angular';
 import { BotMessage, Sakhi } from 'src/app/appConstants';
 import { AppHeaderService, BotApiService, RecordingService, StorageService } from 'src/app/services';
 import { Keyboard } from "@capacitor/keyboard";
@@ -39,7 +39,8 @@ export class BotMessagesComponent  implements OnInit, AfterViewInit {
     private messageApi: BotApiService,
     private translate: TranslateService,
     private telemetryGeneratorService: TelemetryGeneratorService,
-    private storage: StorageService
+    private storage: StorageService,
+    private platform: Platform
   ) { 
     this.defaultLoaderMsg = {identifier: "", message: this.translate.instant('Loading...'), messageType: 'text', displayMsg: this.translate.instant('Loading...'), type: 'received', time: '', timeStamp: '', readMore: false, likeMsg: false, dislikeMsg: false, requestId: ""};
     this.botMessages = [];
@@ -48,6 +49,9 @@ export class BotMessagesComponent  implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.initialiseBot();
+    this.platform.backButton.subscribeWithPriority(11, async () => {
+      this.handleBackNavigation();
+    });
     this.headerService.headerEventEmitted$.subscribe((name: any) => {
       if (name == "back" && !this.navigated) {
         this.navigated = true;
