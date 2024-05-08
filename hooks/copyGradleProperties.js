06367eb5abc9b6cf3eab21c1de2b6455fc5ed5ea
swayangjit <swayangjit029@gmail.com>
2024-05-08 13:31:16 +0530

    Issue #DJP-000 chore: Updated scripts

--- hooks/copyGradleProperties.js
+++ hooks/copyGradleProperties.js
@@ -10,10 +10,6 @@ const appid = properties['app_id'];
 const verCode = properties['app_version_code'];
 
 console.log("****** gradle properties ", properties);
-let appId = `applicationId "${appid}"`;
-let appendStr = '\t\tapplicationId app_id \n' +
-    '\t\tresValue("string", "app_name", "${app_name}") \n' +  
-    '\t\tresValue("string", "app_id", "${app_id}")'
 let androidbuild = "android/app/build.gradle";
 let appendStrCode = `\t\tversionCode ${verCode}`
 
@@ -23,7 +19,7 @@ fs.readFile("android/gradle.properties", "utf-8", (err, data) => {
         console.error(err);
         return;
     }
-    if(data.match("# config.properties")) {
+    if (data.match("# config.properties")) {
         console.log("exist ");
     } else {
         fs.readFile("configuration/config.properties", 'utf8', (err, data) => {
@@ -36,44 +32,21 @@ fs.readFile("android/gradle.properties", "utf-8", (err, data) => {
     }
 })
 
- // build gardle fix
+// build gardle fix
 fs.readFile(androidbuild, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
     }
     let arr = data.split('\n');
-    let exists = false;
-    arr.forEach((a, i)=> {
-        if(a.match(appId)) {
-            arr[i] = appendStr
-        }
-        if(a.match('versionCode') && !a.match(appendStrCode)) {
+    arr.forEach((a, i) => {
+        if (a.match('versionCode') && !a.match(appendStrCode)) {
             arr[i] = appendStrCode
-        }
-        if(a.match("signingConfigs {")) {
-            exists = true;
-        }
-        if(a.match("minifyEnabled false")) {
-            arr[i] = 
-            `signingConfig signingConfigs.release
-            minifyEnabled true`
-        }
-        if(a.match('buildTypes {') && !exists) {
-            arr[i] = `signingConfigs {
-                release {
-                    storeFile = file("keystore/android_keystore.jks")
-                    storePassword System.getenv("SIGNING_STORE_PASSWORD")
-                    keyAlias System.getenv("SIGNING_KEY_ALIAS")
-                    keyPassword System.getenv("SIGNING_KEY_PASSWORD")
-                }
-            }
-            buildTypes {`
         }
     })
     fs.writeFile(androidbuild, arr.join("\n"), (err) => {
         if (err) {
-        console.error("********* err", err);
+            console.error("********* err", err);
         }
     });
 });
@@ -81,7 +54,7 @@ fs.readFile(androidbuild, 'utf8', (err, data) => {
 function readPropertiesFile(filePath) {
     const content = fs.readFileSync(filePath, 'utf-8');
     const lines = content.split('\n');
-    
+
     const properties = {};
 
     for (const line of lines) {
@@ -96,20 +69,3 @@ function readPropertiesFile(filePath) {
 
     return properties;
 }
-
-fs.readFile("android/variables.gradle", 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    if(data.match("minSdkVersion = 22")) {
-        console.log("exist ");
-        let updatedData = data.replace('minSdkVersion = 22', 'minSdkVersion = 23')
-        fs.writeFile("android/variables.gradle", updatedData, (err) => {
-            if (err) {
-                console.error("********* err", err);
-            }
-        });
-    }
-
-})
